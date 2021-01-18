130b4dfc4304d9c7473a682073737901d465e64b
Yaroslav Baronin <baronin.yaroslav@gmail.com>
2021-01-18 22:01:02 +0200

    added page lund

---
+++ src/components/pages/lund.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import WeeksChallenge from "../weeks-challenge";
+const Lund = () => {
+    return (
+        <div className="cart">
+            <WeeksChallenge/>
+        </div>
+    )
+}
+
+export default Lund;
